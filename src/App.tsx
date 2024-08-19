03f26d8b9294917ab1b83f17906aa14b6e4e1b97
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-08-19 18:34:55 +0430

    Exposing Component APIs with useImperativeHandle with TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import Input from "./components/Input";
-import Form from "./components/Form";
+import Form, { type FormHandle } from "./components/Form";
 import Button from "./components/Button";
+import { useRef } from "react";
 
 function App() {
+  // We are telling here that useRef should expect a type of FormHandle.
+  const customForm = useRef<FormHandle>(null);
+
   function handleSave(data: unknown) {
     // We use {as} keyword when we know better the type of something than typescript and with this approach we just annotate this to typescript.
-    const extractedData = data as {name: string, age: string};
+    const extractedData = data as { name: string; age: string };
     console.log(extractedData);
+    customForm.current?.clear();
   }
-  
+
   return (
     <main>
-      <Form onSave={handleSave}>
+      <Form onSave={handleSave} ref={customForm}>
         <Input type="text" label="Name" id="name" />
         <Input type="number" label="Age" id="age" />
         <p>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,14 @@
-import { type FormEvent, type ComponentPropsWithoutRef } from "react";
+import {
+  type FormEvent,
+  type ComponentPropsWithoutRef,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+} from "react";
+
+export type FormHandle = {
+  clear: () => void;
+};
 
 // This type will allows all the props that a form can have.
 type FormProps = ComponentPropsWithoutRef<"form"> & {
@@ -6,20 +16,41 @@ type FormProps = ComponentPropsWithoutRef<"form"> & {
   onSave: (value: unknown) => void;
 };
 
-export default function Form({ onSave, children, ...otherProps }: FormProps) {
+// forwardRef tells that our component is able to be rferred in another components. 
+const Form = forwardRef<FormHandle, FormProps>(function Form(
+  { onSave, children, ...otherProps },
+  ref
+) {
+  const form = useRef<HTMLFormElement>(null);
+
+  // This hook helps us to create a function in our custom component which we can call outside that component in other components.
+  // Which in this case the clear method which clears the form after clicking the save button.
+  // and also this hook take two parameters.
+  useImperativeHandle(ref, () => {
+    return {
+      clear() {
+        console.log("CLEARING");
+        form.current?.reset();
+      },
+    };
+  });
+
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
     const formData = new FormData(event.currentTarget);
-    
     // converting formData to a simpler object to be able to access specific data with {data.name} syntax.
     const data = Object.fromEntries(formData);
     onSave(data);
+    // this will reset the form.
+    form.current?.reset();
   }
 
   return (
-    <form onSubmit={handleSubmit} {...otherProps}>
+    <form onSubmit={handleSubmit} {...otherProps} ref={form}>
       {children}
     </form>
   );
-}
+});
+
+export default Form;
