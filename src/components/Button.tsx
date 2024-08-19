fd28451d66e585b22fe62bf72e349265053d81eb
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-08-19 09:38:13 +0430

    Working with Type Predicates Facing TypeScript Limitations

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,22 @@
-import { ComponentPropsWithoutRef } from "react";
+import { type ComponentPropsWithoutRef } from "react";
 
-type ButtonProps = {
-  el: "button";
-} & ComponentPropsWithoutRef<"button">;
+type ButtonProps = ComponentPropsWithoutRef<"button"> & {
+  // Here href is optional but if it's used it's type is never that tells this should not be set to any value.
+  href?: never;
+};
 
-type AnchorProps = {
-  el: "anchor";
-} & ComponentPropsWithoutRef<"a">;
+type AnchorProps = ComponentPropsWithoutRef<"a"> & {
+  // Here href is optional and if it's used it's type is string.
+  href?: string;
+};
+
+// This function return true if href is in the props and the annotation after paranthesis demonstrates that if the function returns true then the props is a type of AnchorProps. {props is AnchorProps} in typescript is a specific syntax that is called predicate.
+function isAnchorProps(props: ButtonProps | AnchorProps): props is AnchorProps {
+  return "href" in props;
+}
 
 export default function Button(props: ButtonProps | AnchorProps) {
-  if (props.el === "anchor") {
+  if (isAnchorProps(props)) {
     return <a className="button" {...props}></a>;
   }
 
