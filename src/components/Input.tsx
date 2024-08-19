c85a9873368a5a3f6e95310c7579486f4bf0e3a3
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-08-19 07:28:30 +0430

    ComponentPropsWithoutRef generic type added for setting type for all the possible props that an input element can have.

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,18 @@
+import { ComponentPropsWithoutRef } from "react";
+
+// Merging custom props types with predefined input element props types using ComponentPropsWithoutRef generic type. thus we can use other props for our custom Input component.
 type InputProps = {
   label: string;
   id: string;
-};
+} & ComponentPropsWithoutRef<"input">;
 
-export default function Input({ label, id }: InputProps) {
+// Destructuring all possible props that an input element can have with ...props.
+export default function Input({ label, id, ...props }: InputProps) {
   return (
     <p>
       <label htmlFor={id}>{label}</label>
-      <input id={id} type="text" />
+      {/* using {...props} to get all possible remained props. */}
+      <input id={id} type="text" {...props} />
     </p>
   );
 }
