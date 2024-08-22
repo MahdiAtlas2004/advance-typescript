8bfa40c3a1e566895f5e0d738df9beed6ff138b1
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-08-22 14:06:44 +0430

    Accessing Context TypeSafe With A Custom Hook

--- src/store/timers-context.tsx
+++ src/store/timers-context.tsx
@@ -1,4 +1,4 @@
-import { createContext, type ReactNode } from "react";
+import { createContext, useContext, type ReactNode } from "react";
 
 // these are the types of the data object.
 type Timer = {
@@ -15,21 +15,35 @@ type TimersState = {
 // these are the types of methods that should be managed by context.
 // over all this merged object is the thing that is accessible across the application and let the other components to interact with data.
 type TimersContextValue = TimersState & {
-  addTimer: (timerData: Timer) => void,
-  startTimers: () => void,
-  stopTimers: () => void,
+  addTimer: (timerData: Timer) => void;
+  startTimers: () => void;
+  stopTimers: () => void;
 };
 
 // This is the basic object that create a context which then let us use our data across the application.
 const TimersContext = createContext<TimersContextValue | null>(null);
 
+// we create our own custom hook to use it on the other components that need the context values.
+export function useTimersContext() {
+  const timersCtx = useContext(TimersContext);
+
+  // This will always make sure that the context value is not null.
+  if (timersCtx === null) {
+    throw new Error("TimersContext is null - that should not be the case!");
+  }
+
+  return timersCtx;
+}
+
 // This type is for components that will be wraped by TimersContextProvider function.
 type TimersContextProviderProps = {
   children: ReactNode;
-}
+};
 
 // With this special component function we can now wrap other components of our application that then they will be able to access these values.
-export default function TimersContextProvider({children}: TimersContextProviderProps) {
+export default function TimersContextProvider({
+  children,
+}: TimersContextProviderProps) {
   const ctx: TimersContextValue = {
     timers: [],
     isRuning: false,
@@ -41,8 +55,10 @@ export default function TimersContextProvider({children}: TimersContextProviderP
     },
     stopTimers() {
       //...
-    }
+    },
   };
   // Here Provider object needs initial value that's why we are creating ctx object and these values then will be accessible for the other components.
-  return <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>
+  return (
+    <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>
+  );
 }
