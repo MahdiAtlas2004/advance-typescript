609c83b6ac82e84b82f89cc6a650a92d207fc56f
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-08-24 07:01:04 +0430

    Using useEffect with TypeScript

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,19 +1,27 @@
-import Container from './UI/Container.tsx';
-import { type Timer as TimerProps } from '../store/timers-context.tsx';
-import { useState } from 'react';
+import Container from "./UI/Container.tsx";
+import { type Timer as TimerProps } from "../store/timers-context.tsx";
+import { useEffect, useState } from "react";
 
-export default function Timer({name, duration}: 
-TimerProps) {
+export default function Timer({ name, duration }: TimerProps) {
   const [remainingTime, setRemainingTime] = useState(duration * 1000);
 
-  setInterval(function() {
-    setRemainingTime(prevTime => prevTime - 50);
-  }, 50);
+  // Without useEffect the setInterval function will run every time the state change again and it cause an infinite loop there for we use useEffect to prevent this behavior.
+  useEffect(() => {
+    setInterval(function () {
+      setRemainingTime((prevTime) => prevTime - 50);
+    }, 50);
+  }, []);
+
+  // converting mili second to second.
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p><progress max={duration * 1000} value={remainingTime} /></p>
-      <p>{}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 }
