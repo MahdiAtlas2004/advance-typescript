78e3eea19302dec80ea6b1b2944ab5e4c817dd6e
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-08-25 16:40:27 +0430

    useEffect Its Dependencies

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,15 @@
 import Container from "./UI/Container.tsx";
-import { type Timer as TimerProps } from "../store/timers-context.tsx";
+import {
+  useTimersContext,
+  type Timer as TimerProps,
+} from "../store/timers-context.tsx";
 import { useEffect, useRef, useState } from "react";
 
 export default function Timer({ name, duration }: TimerProps) {
   // the generic type tell the typescript that at first the value of useRef is null but eventually it's a number.
   const interval = useRef<number | null>(null);
   const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  const { isRuning } = useTimersContext();
 
   // stoping timer when it reaches zero
   if (remainingTime <= 0 && interval.current) {
@@ -14,15 +18,19 @@ export default function Timer({ name, duration }: TimerProps) {
 
   // Without useEffect the setInterval function will run every time the state change again and it cause an infinite loop there for we use useEffect to prevent this behavior.
   useEffect(() => {
-    const timer = setInterval(function () {
-      setRemainingTime((prevTime) => prevTime - 50);
-    }, 50);
-    // this will store the value of interval before cleaning it.
-    interval.current = timer;
-
+    let timer: number;
+    if (isRuning) {
+      timer = setInterval(function () {
+        setRemainingTime((prevTime) => prevTime - 50);
+      }, 50);
+      // this will store the value of interval before cleaning it.
+      interval.current = timer;
+    } else if (interval.current) {
+      clearInterval(interval.current);
+    }
     // if a useEffect going to return something it should be a function.
     return () => clearInterval(timer);
-  }, []);
+  }, [isRuning]);
 
   // converting mili second to second.
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
